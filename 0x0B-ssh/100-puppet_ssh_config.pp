d226b934ce868b342e4ee1ec897493343fc817d3
crownpoz <agbeyoadebimpe@gmail.com>
2022-10-16 18:55:55 +0000

    puppet config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# customising config files using puppet
+
+include stdlib
+
+file_line {'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => 'identityFile ~/.ssh/school'
+  replace =>  true,
+}
+
+file_line { 'Turn off passwd auth':
+  path    =>  '/etc/ssh/ssh_config',
+  line    =>  'PasswordAuthentication no',
+  replace =>   true
+}
